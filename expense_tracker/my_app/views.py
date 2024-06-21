c0ba660dc6d4aeda2ab4ccc137384378539628e8
bxs213 <bsaleem@emich.edu>
2024-06-21 13:04:30 -0400

    Starting work on HTML pages

--- expense_tracker/my_app/views.py
+++ expense_tracker/my_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def contact_us(request):
+    return render(request, "contact_us.html")
+
+
+def bootstrap(request):
+    return render(request, "bootstrap.html")
